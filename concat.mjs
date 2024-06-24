3819efeaf82f22bdebdcd7f5e512a67fb5f06c36
Yanice <git@yanice.me>
2024-06-24 15:19:11 +0200

    can now do archive properly

--- concat.mjs
+++ concat.mjs
@@ -2,9 +2,14 @@
 // USED TO CREATE THE TOPICS FILE CONTAINING ALL OF THE TOPICS
 // USED FOR THE API TO DOWNLOAD FILES
 
-import * as crime from "./formatted/crime.json" assert { type: "json"}
-import * as apt from "./formatted/apts.json" assert { type: "json"}
-import fs from 'fs'
+import * as crime from "./formatted/crime.json" assert { type: "json" };
+import * as apt from "./formatted/apts.json" assert { type: "json" };
+import * as archive from "./formatted/archive.json" assert { type: "json" };
+import * as papers from "./formatted/papers.json" assert { type: "json" };
+import fs from "fs";
 
-let concattedArray = apt.default.concat(crime.default)
-fs.writeFile("./topics.json", JSON.stringify(concattedArray), () => {})
+let concattedArray = apt.default
+	.concat(crime.default)
+	.concat(archive.default)
+	.concat(papers.default);
+fs.writeFile("./topics.json", JSON.stringify(concattedArray), () => {});
--- parseTorrentFiles.mjs
+++ parseTorrentFiles.mjs
@@ -2,37 +2,49 @@
  * Usage: INPUT=crime node parseTorrentFiles.mjs
  */
 
-import parseTorrent from 'parse-torrent'
-import fs from "fs"
+import parseTorrent from "parse-torrent";
+import fs from "fs";
 
-let inputName = process.env.INPUT
+let inputName = process.env.INPUT;
 
-let stuff = await parseTorrent(fs.readFileSync(`./torrents/${inputName}.torrent`))
+let stuff = await parseTorrent(
+	fs.readFileSync(`./torrents/${inputName}.torrent`),
+);
 
-delete stuff.info.files
-delete stuff.info.pieces
-delete stuff.infoBuffer
-delete stuff.pieces
-delete stuff.announce
-delete stuff.info
-delete stuff.infoHashBuffer
+delete stuff.info.files;
+delete stuff.info.pieces;
+delete stuff.infoBuffer;
+delete stuff.pieces;
+delete stuff.announce;
+delete stuff.info;
+delete stuff.infoHashBuffer;
 
-let array = []
+let array = [];
 
 stuff.files.forEach((file) => {
-	let topic = file.path.split("/")[2].replaceAll(".pdf", "")
-
-	let index = array.findIndex((thingies) => thingies.topic === topic)
+	let topic = file.path.split("/")[2].replaceAll(".pdf", ""); // for archive index = 1
+	if (topic == "The Old New Thing")
+		topic = file.path
+			.split("/")[3]
+			.replaceAll(".pdf", "")
+			.replaceAll(".7z", "")
+			.replaceAll(".zip", ""); //remove the old new thing
+	if (topic == "Collections")
+		topic = file.path
+			.split("/")[2]
+			.replaceAll(".pdf", "")
+			.replaceAll(".7z", "")
+			.replaceAll(".zip", "");
+	let index = array.findIndex((thingies) => thingies.topic === topic);
 
 	if (index === -1) {
 		array.push({
 			topic: topic,
-			files: [file.path]
-		})
+			files: [file.path],
+		});
 	} else {
-		array[index].files.push(file.path)
+		array[index].files.push(file.path);
 	}
-})
+});
 
-fs.writeFile(`./formatted/${inputName}.json`, JSON.stringify(array), () => {})
-
+fs.writeFile(`./formatted/${inputName}.json`, JSON.stringify(array), () => {});
